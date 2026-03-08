6cf05bb5c7f710f21560aa225ffd2c740070ac24
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2026-03-08 10:40:47 -0400

    header readout

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <string>
 
 int main() {
+    const int HEADER_LENGTH = 17;
+
     std::ifstream blend("Cube.blend", std::ios::in | std::ios::binary);
 
     char* buffer = new char [17 + 1];
@@ -14,4 +16,7 @@ int main() {
     std::string str = buffer;
 
     std::cout << "header: " << str << "\n";
+    std::cout << "header length: " << str.substr(7, 2) << "\n";
+    std::cout << "format version: " << str.substr(10, 2) << "\n";
+    std::cout << "blender version: " << str.substr(14, 3) << "\n";
 }
