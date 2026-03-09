8d15c95d2901ba0f864feea35ef2f2a67e394f38
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2026-03-08 21:30:15 -0400

    seek next block header

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 
 // Aka BHead or LargeBHead8 in blender source
 struct BlockHeader {
-    int code;
-    int SDNAnr;
+    int32_t code;
+    int32_t SDNAnr;
     uint64_t old_pointer;
     int64_t len;
     int64_t nr;
@@ -21,6 +21,7 @@ struct BlendFile {
     int blender_version;
 
     BlockHeader test_header;
+    BlockHeader test_header2;
 };
 
 
@@ -55,16 +56,24 @@ BlendFile ReadBlendFile(const char* path) {
     result.blender_version = blender_version;
 
     /* ---------------------------- READ BLOCK HEADER --------------------------- */
-    BlockHeader blockHeader;
+    BlockHeader block_header;
 
-    file.read((char*)&blockHeader, sizeof(BlockHeader));
-    result.test_header = blockHeader;
+    file.read((char*)&block_header, sizeof(BlockHeader));
+    result.test_header = block_header;
+
+    // advance to next block
+    file.seekg(block_header.len, std::ios::seekdir::_S_cur);
+
+    BlockHeader block_header2;
+    file.read((char*)&block_header2, sizeof(BlockHeader));
+    result.test_header2 = block_header2;
+
 
     return result;
 }
 
-void Int32ToChar(char a[], int n) {
-    memcpy(a, &n, 4);
+void Int32ToChar(char a[], int32_t n) {
+    memcpy(a, &n, sizeof(int32_t));
 }
 
 int main() {
@@ -75,13 +84,24 @@ int main() {
     std::cout << "length: " << blendFile.header_length << "\n";
     std::cout << "format version: " << blendFile.format_version << "\n";
     std::cout << "blender version: " << blendFile.blender_version << "\n";
-    std::cout << "\n";
-    std::cout << "block:\n";
-    char code[4];
+
+    std::cout << "\nblock:\n";
+    char code[sizeof(int32_t) + 1];
+    code[sizeof(int32_t)] = '\0';
     Int32ToChar(code, blendFile.test_header.code);
     std::cout << "code: " << code << "\n";
     std::cout << "SDNA struct type: " << blendFile.test_header.SDNAnr << "\n";
     std::cout << "old pointer: " << blendFile.test_header.old_pointer << "\n";
     std::cout << "byte length: " << blendFile.test_header.len << "\n";
     std::cout << "number of structs: " << blendFile.test_header.nr << "\n";
+
+    std::cout << "\nblock:\n";
+    char code2[sizeof(int32_t) + 1];
+    code[sizeof(int32_t)] = '\0';
+    Int32ToChar(code2, blendFile.test_header2.code);
+    std::cout << "code: " << code2 << "\n";
+    std::cout << "SDNA struct type: " << blendFile.test_header2.SDNAnr << "\n";
+    std::cout << "old pointer: " << blendFile.test_header2.old_pointer << "\n";
+    std::cout << "byte length: " << blendFile.test_header2.len << "\n";
+    std::cout << "number of structs: " << blendFile.test_header2.nr << "\n";
 }
