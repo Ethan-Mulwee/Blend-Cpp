3e4e9c0542c6b34054a5f9772ef2a62458d0fdd5
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2026-03-19 21:24:31 -0400

    initial changes for cmake

--- demo/main.cpp
+++ demo/main.cpp
@@ -5,6 +5,7 @@
 #include <exception>
 #include <cstdint>
 #include <map>
+#include <vector>
 
 #include "sdna_structs.h"
 #include "attribute_types.h"
@@ -492,16 +493,143 @@ void ExtractSDNATypesToHeaderFile(const BlendFile& blend_file) {
 
     file.close();
 }
-
-void InterpretDataBlocks(const BlendFile& blend) {
+/* TODO: handle pointer mapping better, either remap all the pointers at once 
+or do something else to ensure you can still access other datablock data */
+void InterpretDataBlocks(BlendFile& blend) {
     DataBlockNode* node = blend.block_header_list.first;
+    std::vector<Mesh> meshes = {};
+    std::vector<FileGlobal> fileGlobals = {};
     int uncovered_cases = 0;
     while(node) {
         // switch on type index
         switch (node->block_header.SDNA_type_index) {
 
+            case 0: { // raw_data
+                std::cout << "raw_data found \n";
+                break;
+            }
+
+            case 1: { // byte, char, uint_8, etc. (this is never used by a datablock)
+                std::cout << "byte found \n";
+                break;
+            }
+
+            case 170:  { // FileGlobal
+                std::cout << "FileGlobal found \n";
+                FileGlobal file_global = blend.ReadRawDataAs<FileGlobal>(node->data_offset);
+                std::cout << file_global.subvstr << "\n";
+                break;
+            }
+
             case 321: { // Mesh
                 std::cout << "Found a mesh \n";
+                Mesh mesh; 
+                mesh = ReadDataBlock<Mesh>(blend, node, 0);
+                std::cout << "SDNA index: " << node->block_header.SDNA_type_index << "\n";
+                std::cout << "Mesh Name: " << mesh.id.name << "\n";
+                std::cout << "Vertex Count: " << mesh.totvert << "\n";
+                std::cout << "Edge Count: " << mesh.totedge << "\n";
+                std::cout << "Face Count: " << mesh.totpoly << "\n";
+                std::cout << "Corner Count: " << mesh.totloop << "\n";
+                std::cout << "Number of Attributes:" << mesh.attribute_storage.dna_attributes_num << "\n";
+    
+                DataBlockNode* poly_offset_indices_data_block = blend.MapPointerToBlock(mesh.poly_offset_indices);
+                std::cout << "face_offset_indices Type: " << blend.TypeNameOfDataBlock(poly_offset_indices_data_block) << "\n";
+                std::cout << "face_offset_indices Byte Length: " << poly_offset_indices_data_block->block_header.byte_length << "\n";
+                int* poly_offset_indices = (int*)blend.GetRawDataAddress(poly_offset_indices_data_block->data_offset);
+                /* See DNA_mesh_types.h faces() */
+
+                /* From the developer docs https://developer.blender.org/docs/features/objects/mesh/mesh/ 
+                OffsetIndices is a general abstraction for splitting a larger array into many contiguous groups. 
+                Every group is represented by a single integer-- the first index of the elements in the group. 
+                The end of the group is simply the next integer in the offsets array. For example, the face offsets 
+                [0,3,7,10,14] encode a triangle, a quad, a triangle, and a quad, in that order. */
+
+                /* i.e 7 numbers encode 6 faces explaining the + 1 */
+                std::cout << "{";
+                for (int poly_offset_indice_idx = 0; poly_offset_indice_idx < mesh.totpoly + 1; poly_offset_indice_idx++) {
+                    std::cout << poly_offset_indices[poly_offset_indice_idx] << ", ";
+                }
+                std::cout << "}\n";
+
+                Attribute* attributes = blend.MapPointer(mesh.attribute_storage.dna_attributes);
+                
+                std::cout << "Mesh Attributes: \n";
+                for (int attribute_idx = 0; attribute_idx < mesh.attribute_storage.dna_attributes_num; attribute_idx++) {
+                    Attribute& attribute = attributes[attribute_idx];
+                    std::cout << "    Name: " << blend.MapPointer(attribute.name) << "\n";
+                    std::cout << "    Type: " << attribute.data_type << "\n";
+                    std::cout << "    Domain: " << (int)attribute.domain << "\n";
+
+                    DataBlockNode* attribute_array_data = blend.MapPointerToBlock(attribute.data);
+                    const char* attribute_array_data_type = blend.TypeNameOfDataBlock(attribute_array_data);
+                    AttributeArray attribute_array = ReadDataBlock<AttributeArray>(blend, attribute_array_data, 0);
+
+                    std::cout << "    Data Type: " << attribute_array_data_type << " {\n";
+                    std::cout << "        Size: " << attribute_array.size << "\n";
+                    
+                    // Read raw attribute data bytes
+                    DataBlockNode* raw_data_block = blend.MapPointerToBlock(attribute_array.data);
+                    const char* raw_data_type = blend.TypeNameOfDataBlock(raw_data_block);
+                    raw_data* data = (raw_data*)blend.GetRawDataAddress(raw_data_block->data_offset);
+                    uint32_t byte_length = raw_data_block->block_header.byte_length;
+                    uint32_t number_structs = raw_data_block->block_header.struct_number;
+                    std::cout << "        Data Type: " << raw_data_type << "\n";
+                    std::cout << "        Byte Length: " << byte_length << "\n";
+                    std::cout << "        Number of Structs: " << number_structs << "\n";
+                    
+                    for (int array_idx = 0; array_idx < attribute_array.size; array_idx++) {
+                        switch ((AttrType)attribute.data_type) {
+                            case AttrType::Float3: {
+                                float x, y, z;
+                                int data_idx = array_idx * sizeof(float) * 3;
+                                x = *reinterpret_cast<float*>(&data[data_idx]);
+                                y = *reinterpret_cast<float*>(&data[data_idx + sizeof(float)]);
+                                z = *reinterpret_cast<float*>(&data[data_idx + (sizeof(float) * 2)]);
+                                
+                                std::cout << "        {" << x << ", " << y << ", " << z << "}\n";
+                                break;
+                            }
+
+                            case AttrType::Int32_2D: {
+                                int32_t a, b;
+                                int data_idx = array_idx * sizeof(int32_t) * 2;
+                                a = *reinterpret_cast<int32_t*>(&data[data_idx]);
+                                b = *reinterpret_cast<int32_t*>(&data[data_idx + sizeof(int32_t)]);
+
+                                std::cout << "        {" << a << ", " << b << "}\n";
+                                break;
+                            }
+
+                            case AttrType::Int32: {
+                                int32_t i;
+                                int data_idx = array_idx * sizeof(int32_t);
+                                i = *reinterpret_cast<int32_t*>(&data[data_idx]);
+
+                                std::cout << "        {" << i << "}\n";
+                                break;
+                            }
+                            
+                            case AttrType::Bool: {
+                                bool b;
+                                b = *reinterpret_cast<bool*>(&data[array_idx]);
+
+                                if (b) {
+                                    std::cout << "        {true}\n";
+                                } else {
+                                    std::cout << "        {false}\n";
+                                }
+                            }
+
+                            default:
+                                break;
+                        }
+                    }
+
+
+                    std::cout << "    }\n";
+                }
+                meshes.push_back(mesh);
                 break;
             }
 
@@ -561,122 +689,14 @@ int main() {
     /* See DNA_mesh_types.h struct Mesh */
 
 
-    DataBlockNode* node = blend_file.block_header_list.first;
-    while(node) {
-        const DataBlockHeader& block_header = node->block_header;
-        SDNA_Struct* struct_info = blend_file.sdna->structs[block_header.SDNA_type_index];
-        const char* type_name = blend_file.sdna->types[struct_info->type_index];
-        if (strcmp(type_name, "Mesh") == 0) {
-            Mesh mesh; 
-            mesh = ReadDataBlock<Mesh>(blend_file, node, 0);
-            std::cout << "SDNA index: " << node->block_header.SDNA_type_index << "\n";
-            std::cout << "Mesh Name: " << mesh.id.name << "\n";
-            std::cout << "Vertex Count: " << mesh.totvert << "\n";
-            std::cout << "Edge Count: " << mesh.totedge << "\n";
-            std::cout << "Face Count: " << mesh.totpoly << "\n";
-            std::cout << "Corner Count: " << mesh.totloop << "\n";
-            std::cout << "Number of Attributes:" << mesh.attribute_storage.dna_attributes_num << "\n";
- 
-            DataBlockNode* poly_offset_indices_data_block = blend_file.MapPointerToBlock(mesh.poly_offset_indices);
-            std::cout << "face_offset_indices Type: " << blend_file.TypeNameOfDataBlock(poly_offset_indices_data_block) << "\n";
-            std::cout << "face_offset_indices Byte Length: " << poly_offset_indices_data_block->block_header.byte_length << "\n";
-            int* poly_offset_indices = (int*)blend_file.GetRawDataAddress(poly_offset_indices_data_block->data_offset);
-            /* See DNA_mesh_types.h faces() */
+    // DataBlockNode* node = blend_file.block_header_list.first;
+    // while(node) {
+    //     const DataBlockHeader& block_header = node->block_header;
+    //     SDNA_Struct* struct_info = blend_file.sdna->structs[block_header.SDNA_type_index];
+    //     const char* type_name = blend_file.sdna->types[struct_info->type_index];
 
-            /* From the developer docs https://developer.blender.org/docs/features/objects/mesh/mesh/ 
-            OffsetIndices is a general abstraction for splitting a larger array into many contiguous groups. 
-            Every group is represented by a single integer-- the first index of the elements in the group. 
-            The end of the group is simply the next integer in the offsets array. For example, the face offsets 
-            [0,3,7,10,14] encode a triangle, a quad, a triangle, and a quad, in that order. */
-
-            /* i.e 7 numbers encode 6 faces explaining the + 1 */
-            std::cout << "{";
-            for (int poly_offset_indice_idx = 0; poly_offset_indice_idx < mesh.totpoly + 1; poly_offset_indice_idx++) {
-                std::cout << poly_offset_indices[poly_offset_indice_idx] << ", ";
-            }
-            std::cout << "}\n";
-
-            Attribute* attributes = blend_file.MapPointer(mesh.attribute_storage.dna_attributes);
-            
-            std::cout << "Mesh Attributes: \n";
-            for (int attribute_idx = 0; attribute_idx < mesh.attribute_storage.dna_attributes_num; attribute_idx++) {
-                Attribute& attribute = attributes[attribute_idx];
-                std::cout << "    Name: " << blend_file.MapPointer(attribute.name) << "\n";
-                std::cout << "    Type: " << attribute.data_type << "\n";
-                std::cout << "    Domain: " << (int)attribute.domain << "\n";
-
-                DataBlockNode* attribute_array_data = blend_file.MapPointerToBlock(attribute.data);
-                const char* attribute_array_data_type = blend_file.TypeNameOfDataBlock(attribute_array_data);
-                AttributeArray attribute_array = ReadDataBlock<AttributeArray>(blend_file, attribute_array_data, 0);
-
-                std::cout << "    Data Type: " << attribute_array_data_type << " {\n";
-                std::cout << "        Size: " << attribute_array.size << "\n";
-                
-                // Read raw attribute data bytes
-                DataBlockNode* raw_data_block = blend_file.MapPointerToBlock(attribute_array.data);
-                const char* raw_data_type = blend_file.TypeNameOfDataBlock(raw_data_block);
-                raw_data* data = (raw_data*)blend_file.GetRawDataAddress(raw_data_block->data_offset);
-                uint32_t byte_length = raw_data_block->block_header.byte_length;
-                uint32_t number_structs = raw_data_block->block_header.struct_number;
-                std::cout << "        Data Type: " << raw_data_type << "\n";
-                std::cout << "        Byte Length: " << byte_length << "\n";
-                std::cout << "        Number of Structs: " << number_structs << "\n";
-                
-                for (int array_idx = 0; array_idx < attribute_array.size; array_idx++) {
-                    switch ((AttrType)attribute.data_type) {
-                        case AttrType::Float3: {
-                            float x, y, z;
-                            int data_idx = array_idx * sizeof(float) * 3;
-                            x = *reinterpret_cast<float*>(&data[data_idx]);
-                            y = *reinterpret_cast<float*>(&data[data_idx + sizeof(float)]);
-                            z = *reinterpret_cast<float*>(&data[data_idx + (sizeof(float) * 2)]);
-                            
-                            std::cout << "        {" << x << ", " << y << ", " << z << "}\n";
-                            break;
-                        }
-
-                        case AttrType::Int32_2D: {
-                            int32_t a, b;
-                            int data_idx = array_idx * sizeof(int32_t) * 2;
-                            a = *reinterpret_cast<int32_t*>(&data[data_idx]);
-                            b = *reinterpret_cast<int32_t*>(&data[data_idx + sizeof(int32_t)]);
-
-                            std::cout << "        {" << a << ", " << b << "}\n";
-                            break;
-                        }
-
-                        case AttrType::Int32: {
-                            int32_t i;
-                            int data_idx = array_idx * sizeof(int32_t);
-                            i = *reinterpret_cast<int32_t*>(&data[data_idx]);
-
-                            std::cout << "        {" << i << "}\n";
-                            break;
-                        }
-                        
-                        case AttrType::Bool: {
-                            bool b;
-                            b = *reinterpret_cast<bool*>(&data[array_idx]);
-
-                            if (b) {
-                                std::cout << "        {true}\n";
-                            } else {
-                                std::cout << "        {false}\n";
-                            }
-                        }
-
-                        default:
-                            break;
-                    }
-                }
-
-
-                std::cout << "    }\n";
-            }
-        }
-
-        node = node->next;
-    }
+    //     node = node->next;
+    // }
 
     InterpretDataBlocks(blend_file);
 
