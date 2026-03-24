9c64066cd95d1be26937e42003c58b6c9c9343cb
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2026-03-24 17:08:41 -0400

    Update main.cpp

--- demo/main.cpp
+++ demo/main.cpp
@@ -4,7 +4,11 @@
 #include <cstring>
 
 int main() {
-
+    // TODO: get rid of monolithic blend file structs, instead split into multiple functions and structs
+    // 1. function for reading and returning the data and info of the raw bytes of the file
+    // 2. function for parsing out the list of data blocks, with the data copied and split into individual buffers for each block
+    // 3. function for sorting the data blocks into lists by type
+    // 4. function for processing the pointers contain within the data block data to remap them to the correct buffers
     BlendFileReader blend_file_reader = ReadBlendFile("Cube.blend");
     // ExtractSDNATypesToHeaderFile(blend_file);
 
